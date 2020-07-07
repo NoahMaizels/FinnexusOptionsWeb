305a962f74e1fca4475650255cf219bca9e7eb86
lolieatapple <lolieatapple@gmail.com>
2020-07-07 11:00:49 +0800

    add error catch.

--- layouts/index.js
+++ layouts/index.js
@@ -33,6 +33,16 @@ class Layout extends Component {
     window.open("https://github.com/wandevs/wan-game/blob/master/GameRule.md");
   }
 
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    // You can also log the error to an error reporting service
+    console.log(error, info);
+  }
+
   render() {
     let nodeUrl = getNodeUrl();
     if (!isSwitchFinish()) {
